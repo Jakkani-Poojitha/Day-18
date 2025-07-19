68df7d392cf32038dcbb9ba514b9d59ff08a5a15
poojitha <jakkanipoojitha@.com>
2025-07-19 14:49:02 +0530

    Inital commit of hello_app

---
+++ hello_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("Hello Coding Experts 👋")
+st.markdown(
+    """ 
+    This is a best class of CSE-3.
+    
+    **There's :rainbow[so much] you can build!**
+    
+    best batch of CSE-3
+    """
+)
+
+if st.button("Send balloons!"):
+    st.balloons()
